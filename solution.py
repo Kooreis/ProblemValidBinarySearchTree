8a772909cbb6f363bbda049849893cc0d05f37cd
Kooreis <kooreis.official@gmail.com>
2024-05-25 02:22:24 -0500

    This update adds the is_bst function which checks if a binary tree is a binary search tree. It does this by recursively checking that all nodes in the left subtree are less than the root and all nodes in the right subtree are greater than the root.

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
-class Node:
-    def __init__(self, key):
-        self.left = None
-        self.right = None
-        self.val = key
+def is_bst(node, min_val=float('-inf'), max_val=float('inf')):
+    if node is None:
+        return True
+    if not min_val <= node.val <= max_val:
+        return False
+    return is_bst(node.left, min_val, node.val) and is_bst(node.right, node.val, max_val)
